653c631b8e7d896691131611f85c9499e03eaa8b
Shuvo-TD <shuvotalukder7466@gmail.com>
2023-10-21 11:30:06 +0600

    feat(components): added root export

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as ChannelCard } from './channelCard';
+export { default as VideoCard } from './videoCard';
+export { default as ChannelDetail } from './channeldetails';
+export { default as Feed } from './feed';
+export { default as Loader } from './loader';
+export { default as Navbar } from './nav';
+export { default as SearchBar } from './search';
+export { default as VideoDetail } from './videoDetails';
+export { default as SearchFeed } from './searchFeed';
+export { default as Videos } from './videos';
+export { default as Sidebar } from './sideBar';
